4b2a15bf4f8f8c2aa5b2b5328f34f28df671f29c
kailashsubash0 <kailashsubash0@gmail.com>
2022-07-25 10:38:33 +0530

    mano code

--- src/test/java/org/test/pages/FaceBook.java
+++ src/test/java/org/test/pages/FaceBook.java
@@ -2,6 +2,9 @@ package org.test.pages;
 
 public class FaceBook {
 	public static void main(String[] args) {
+		System.out.println("test 1");
+		System.out.println("test 2");
+		System.out.println("test 3");
 		
 	}
 
